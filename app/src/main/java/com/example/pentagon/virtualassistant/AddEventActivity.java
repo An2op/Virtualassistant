e75765d42adb5563a52e853c3e04d2a6c46fc05c
LAPIS-ANTONY <an2opvs@gmail.com>
2018-04-13 09:50:58 +0530

    test4132018

--- app/src/main/java/com/example/pentagon/virtualassistant/AddEventActivity.java
+++ app/src/main/java/com/example/pentagon/virtualassistant/AddEventActivity.java
@@ -1,13 +1,59 @@
 package com.example.pentagon.virtualassistant;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class AddEventActivity extends AppCompatActivity {
-
+EditText voiceInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        voiceInput=(EditText)findViewById(R.id.editText2);
+
+    }
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Hi speak something");
+        try {
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    // Receiving speech input
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 100: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    voiceInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }}
+
+    public void speek(View view) {
+        askSpeechInput();
     }
 }
